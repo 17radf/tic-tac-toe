11eb75e6ce6210a28054b235f06bab32a95e482b
Raihan <12584890+17radf@users.noreply.github.com>
2022-01-16 18:31:31 +0700

    some fixes and refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,4 @@
 "use strict";
-(function(){
-
-    const container = document.querySelector(".container")
-    for(let i = 0; i < 9; i++){
-        const div = document.createElement('div');
-        div.classList.add('square');
-        div.setAttribute("data", i)
-        div.addEventListener("click", () => {
-            game.setBoard(i)
-        }, {once: true})
-        container.appendChild(div);
-    }
-
-})()
 
 const player = (name, marker) => {
     return { name, marker }
@@ -36,32 +22,58 @@ let game = ((playerOne, playerTwo) => {
     ]
 
     let _currentState = playerOne
+    const _results = document.querySelector(".results")
+    const _turns = document.querySelector(".turns")
+    const _container = document.querySelector(".container")
 
-    function setBoard (where) { 
+    function initGame(){
+
+        for(let i = 0; i < 9; i++){
+            const div = document.createElement('div');
+            div.classList.add('square');
+            div.setAttribute("data", i)
+            div.addEventListener("click", () => {
+                _setBoard(i)
+            }, {once: true})
+            _container.appendChild(div);
+        }
+
+    }
+
+    function _setBoard (where) { 
         _gameBoard[where] = _currentState.marker
 
         const dataDom = document.querySelector(`[data="${where}"]`)
         dataDom.textContent = _currentState.marker
 
-        checkWin()
-        switchState()
+        _checkWin()
+        _switchState()
     }
 
-    function checkWin () {
+    function _switchState() {
+        _currentState = _currentState === playerOne ? playerTwo : playerOne
+        _turns.textContent = `it's ${_currentState.name} turn now!`
+    }
+
+    function _checkWin() {
         for (let i = 0; i < _winningState.length; i++) {
             const array = _winningState[i]
             if(_gameBoard[array[0]] === _currentState.marker &&
                _gameBoard[array[1]] === _currentState.marker &&
                _gameBoard[array[2]] === _currentState.marker ) {
-                   alert(`${_currentState.name} wins`)
+                   endGame()
                }
         }
     }
 
-    function switchState () {
-        _currentState = _currentState === playerOne ? playerTwo : playerOne
+    function endGame() {
+        _results.textContent = `${_currentState.name} wins`
+        _container.style.display = "none"
+        _turns.style.display = "none"
     }
     
-    return { setBoard } 
+    return { initGame } 
 
 })(one, two)
+
+game.initGame()
